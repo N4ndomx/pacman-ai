c9fa8370ebae32718ab08ad51e4becf1f31489ff
Fernando Ramirez <90127607+N4ndomx@users.noreply.github.com>
2024-06-06 02:55:48 -0600

    imple maze gn

--- src/app/services/tablero.service.ts
+++ src/app/services/tablero.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableroService {
-  oneBlockSize = 20;
-  private wallSpaceWidth = this.oneBlockSize / 1;
-  private wallOffset = (this.oneBlockSize - this.wallSpaceWidth) / 2;
+  private apiUrl = 'http://127.0.0.1:8000/generate_map';
+  oneBlockSize = 21;
+  wallSpaceWidth = this.oneBlockSize / 1.6;
+  wallOffset = (this.oneBlockSize - this.wallSpaceWidth) / 3;
   wallInnerColor = "black";
-  puntaje_ganar = 0
+
+  bolitas_totales = 0
+  currentLevel = 0;
 
   map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -36,12 +40,23 @@ export class TableroService {
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ];
 
-  constructor() {
-    this.puntaje_ganar = this.countTwos()
-    console.log(this.puntaje_ganar)
+  constructor(private http: HttpClient) {
+
   }
 
-  countTwos(): number {
+  async getMapAPI(): Promise<void> {
+
+    const response: any = await this.http.get(this.apiUrl, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).toPromise();
+    this.map = response.map
+    this.bolitas_totales = this.bolitas_ganar()
+    // console.log(this.map)
+  }
+
+  bolitas_ganar(): number {
     let count = 0;
     for (let i = 0; i < this.map.length; i++) {
       for (let j = 0; j < this.map[0].length; j++) {
@@ -51,5 +66,13 @@ export class TableroService {
       }
     }
     return count;
+  }
+
+
+  async nextLevel() {
+    this.currentLevel++;
+    await this.getMapAPI()
+    console.log("You have completed all levels!");
+    // Implement game over logic or reset to level 1
   }
 }
